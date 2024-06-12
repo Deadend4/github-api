934647e688a5de2e055613db753b335bcd2bad0f
Саня <70769398+Deadend4@users.noreply.github.com>
2024-06-13 00:13:12 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Octokit } from 'octokit';
+import express from 'express';
+import dotenv from "dotenv";
+
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
+const app = express();
+const port = process.env.PORT || 3001;
+const authToken = process.env.AUTH_TOKEN || "";
+const octokit = new Octokit({ 
+  auth: authToken,
+});
+
+app.listen(port, () => {
+  console.log("Server is listening on port: ", port);
+});
+
+app.get("/home", (req, res) => {
+  res.send("Homepage");
+});
+
+app.get("/me", async (req, res) => {
+  try {
+    const result = await octokit.request("GET /users/{username}", {
+      username: "Deadend4",
+      per_page: 2,
+    });
+  
+    res.send(result.data);
+  
+  } catch (error) {
+    console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+  }
+})
