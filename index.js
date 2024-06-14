b7f9eb02daff77e691525ad1ac3494fb9caf6001
deadend4 <vistanciya.toxin@mail.ru>
2024-06-14 14:03:22 +0300

    add database

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Octokit } from 'octokit';
 import express from 'express';
-import dotenv from "dotenv";
-
+import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -9,29 +8,30 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express();
 const port = process.env.PORT || 3001;
-const authToken = process.env.AUTH_TOKEN || "";
-const octokit = new Octokit({ 
+const authToken = process.env.GITHUB_AUTH_TOKEN || '';
+const octokit = new Octokit({
   auth: authToken,
 });
 
 app.listen(port, () => {
-  console.log("Server is listening on port: ", port);
+  console.log('Server is listening on port: ', port);
 });
 
-app.get("/home", (req, res) => {
-  res.send("Homepage");
+app.get('/home', (req, res) => {
+  res.send('Homepage');
 });
 
-app.get("/me", async (req, res) => {
+app.get('/me', async (req, res) => {
   try {
-    const result = await octokit.request("GET /users/{username}", {
-      username: "Deadend4",
+    const result = await octokit.request('GET /users/{username}', {
+      username: 'Deadend4',
       per_page: 2,
     });
-  
+
     res.send(result.data);
-  
   } catch (error) {
-    console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`)
+    console.log(
+      `Error! Status: ${error.status}. Message: ${error.response.data.message}`
+    );
   }
-})
+});
